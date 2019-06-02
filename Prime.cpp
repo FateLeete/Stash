378ed61645a41f2d6cd00ed26bed3f0587047ea8
Fate Leete <51298257+FateLeete@users.noreply.github.com>
2019-06-02 15:14:11 -0400

    Add files via upload

---
+++ Prime.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+	{
+	long long dNum = 4;
+	long long dCmp = 4;
+
+	cout << ' ' << '1' << ' ' << '1' << endl;
+	cout << ' ' << '2' << ' ' << '2' << endl;
+	cout << ' ' << '3' << ' ' << '3' << endl;
+
+	
+	for (;;)
+		{
+		for (long long i = 2; i < dNum; i++)
+			{
+			if (dNum == 38)
+				{
+				cin.get();
+				break;
+				}
+			if (dNum % i == 0)
+				{
+				break;
+				}
+			if (i > (dNum / 2) + 1 )
+				{
+				cout << ' ' << dCmp << ' ' << dNum << endl;
+				dCmp++;
+				break;
+				}
+			}
+
+		dNum++;
+		}
+
+	return 0;
+	}
+
