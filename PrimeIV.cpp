0ebfc65263d39bad424f05ae5f57ac55d89f7a19
Fate Leete <51298257+FateLeete@users.noreply.github.com>
2020-04-24 15:49:05 -0400

    Add files via upload

--- PrimeIV.cpp
+++ PrimeIV.cpp
@@ -6,17 +6,22 @@ using namespace std;
 int main()
 	{
 	unsigned __int64 dNum = 18446744073709551000ULL;
+	unsigned __int64 dNum2 = 0;
 	
 	for (;;)
 		{
 		cout << "Computing ! ";	
 		for (unsigned __int64 i = 2ULL; i < dNum; i++)
 			{
-			if (dNum % i == 0)
-				{
+			dNum2 = dNum % 10;
+			
+			if (dNum2 == 0 || dNum2 == 2 || dNum2 == 4 || dNum2 == 5 || dNum2 == 6 || dNum2 == 8)
 				break;
-				}	
-            		if ( (double)i > sqrt((double)dNum) )	
+								
+			if (dNum % i == 0)
+				break;
+					
+            if ( (double)i > sqrt((double)dNum) )	
 				{
 				cout << "\n\n********************\n" << dNum << "\n********************\n\n";
 				break;
