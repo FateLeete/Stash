a7b49a788396c12be161ff6761099777e7867232
Fate Leete <51298257+FateLeete@users.noreply.github.com>
2021-02-25 11:48:43 -0500

    Add files via upload

---
+++ BIGCARR.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+	{
+		unsigned __int64 dNum = 0ULL;
+		
+		a= dnum - 1;
+		
+		cout << "The magic # is -------> " << a;
+		
+		return a;		
+	}
