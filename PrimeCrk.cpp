05b0fade5a691abefcc89ac94b3ea1494889901a
Fate Leete <51298257+FateLeete@users.noreply.github.com>
2021-05-27 12:10:43 -0400

    Add files via upload

---
+++ PrimeCrk.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+	{
+	for (;;)
+		{	 
+		bool ss = false;
+		
+		unsigned long long j = 0ULL, dNum = 0ULL, trS = 0ULL ;
+		
+		trS = trS - 1;
+		
+		do
+			{
+			cout << "\n\n\n*****************************************************\n* Enter a number between 3 and " << trS << " : ";
+			cin >> dNum ;
+			}
+		while (dNum < 3 || dNum > trS);
+		
+		cout << "\n\n\n*** ==>  " << dNum << "..   ";		
+		
+		for (;;)
+			{
+			cout << "Computing ! ";	
+			for (unsigned long long i = 2LL; i < dNum; i++)
+				{
+				if (dNum % i == 0)
+					{
+					j = dNum / i ;
+				    cout << "\n\n************************\n" << i << " * " << j << "\n************************";
+				    ss = true;
+				    j = 0;
+					}	
+	            if ( (double)i > sqrt((double)dNum) )	
+					{
+					if (!ss)
+					cout << "\n\n****************\n* Prime Number * " << dNum << "\n****************";
+					j = 1;
+					break;
+					}
+				}
+				
+	        if (j != 0 ) 
+				break;
+			}
+		}
+	
+	return 0;
+	}
+
