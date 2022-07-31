412ddf0ca70cb87195f3212abc69f923117ba00d
Fate Leete <51298257+FateLeete@users.noreply.github.com>
2022-07-31 12:32:52 -0400

    Add files via upload

---
+++ PrimeCPatched.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+	{
+        bool ss = false; 
+        
+	unsigned long long j = 0ULL, dNum = 0ULL, trS = 0ULL ;
+        trS = trS - 1;
+
+	for (;;)
+		{	 
+		ss = false; 
+                j = 0ULL;
+                dNum = 0ULL;
+
+		do
+			{
+			cout << "\n\n\n*****************************************************\n* Enter a number between 3 and " << trS << " : ";
+			cin >> dNum ;
+			}
+		while (dNum < 3 || dNum > trS);
+		
+		cout << "\n\n\n*** ==>  " << dNum << "..   ";		
+		
+		for (;;)
+			{
+			cout << "Computing ! ";	
+			for (unsigned long long i = 2ULL; i < dNum; i++)
+				{
+				if (dNum % i == 0)
+					{
+					j = dNum / i ;
+						if (dNum == 12 && i == 4) break;  // patch
+				        cout << "\n\n************************\n" << i << " * " << j << "\n************************";
+				        if (dNum == 6) break;            //  patch
+						ss = true;
+				        j = 0;
+					}	
+	                        if ( (double)i > sqrt((double)dNum) )	
+					{
+					if (!ss)
+					cout << "\n\n****************\n Prime Number = " << dNum << "\n****************";
+					
+                                        j = 1;
+					break;
+					}
+				}
+				
+	                if (j != 0 ) 
+				break;
+			}
+		}
+	
+	return 0;
+	}
+
