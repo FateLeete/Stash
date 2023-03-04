63fcf8b963c86001038390c7f1ee059007aada2b
Fate Leete <51298257+FateLeete@users.noreply.github.com>
2023-03-04 10:03:15 -0500

    Add files via upload

---
+++ randpro.cpp
@@ -0,0 +1,174 @@
+#include <iostream      // cout cin
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+using namespace std;
+
+const int nbValue = 100000;
+
+int tbRand[nbValue];
+int tbRandRes[100];
+int tbMoy[100];  //
+
+int ct = 0;
+
+bool tbSqr[100];
+
+bool firstLast = true;
+bool firstPrint = true;
+
+int lessOcc = 1000;
+int plusOcc = 1000;
+int lessMoy = 1000;
+int plusMoy = 1000;
+
+int main()
+{
+    
+for(;;)
+{   
+    srand (time(NULL));
+	int tempo = 0;
+	  
+    for (int i = 0; i < nbValue ; i++)
+    {
+        tbRand[i] = rand() % 100;
+    }
+
+    for (int i = 0 ; i < 100; i++)
+    {
+        tbRandRes[i] = 0;
+        tbSqr[i] = false;  //
+    }
+
+	ct = 0;
+
+    for (int i = 0; i < nbValue ; i++)
+    {
+    	tempo = tbRand[i];
+        tbRandRes[tempo] = tbRandRes[tempo] + 1;
+                
+		if (tbRandRes[tempo] == 1000) 
+			{
+			tbSqr[tempo] = true;  //
+			ct++;
+		
+			if (tbRandRes[tempo] > 1000)
+				{
+				tbSqr[tempo] = false;  //     
+ 				ct--;
+				}
+			}
+	}
+    
+    lessOcc = 1000;
+    plusOcc = 1000; 
+    
+    lessMoy = 1000;
+    plusMoy = 1000;
+        
+    if (firstLast)
+    {              
+          for (int i = 0 ; i < 100 ; i++)
+          {   
+              tbMoy[i] = tbRandRes[i];        
+              
+              if (tbRandRes[i] < lessOcc)
+                 {
+                    lessOcc = tbRandRes[i];
+                 }
+              if (tbRandRes[i] > plusOcc)
+                 {
+                    plusOcc = tbRandRes[i];
+                 }       
+          }
+    }
+    else
+    {
+        for (int i = 0 ; i < 100 ; i++)
+        {   
+            tbMoy[i] = ((tbRandRes[i] + tbMoy[i]) / 2);
+               
+            if (tbRandRes[i] < lessOcc)
+            {
+                lessOcc = tbRandRes[i];
+            }
+            if (tbRandRes[i] > plusOcc)
+            {
+                plusOcc = tbRandRes[i];
+            }
+            if (tbMoy[i] < lessMoy)
+            {
+                lessMoy = tbMoy[i];
+            }
+            if (tbMoy[i] > plusMoy)
+            {
+                plusMoy = tbMoy[i];
+            }
+        }    
+	}
+        
+    for (int i = 0 ; i < 100 ; i++)
+    {
+        if (!firstPrint)
+        {
+            if ((i + 2) % 2 == 0 )
+            {
+                 cout << endl;
+            }
+                           
+            cout << "#" << i << "\t" << tbRandRes[i] << "\tMoy:\t" << tbMoy[i] << "\t";
+           
+            if (i == 63)
+            {
+                 cout << "-Occ : " << lessOcc;
+            }
+            if (i == 65)
+            {
+                 cout <<"+Occ : " << plusOcc;
+            }
+            if (i == 67)
+            {
+                 cout << "-Moy : " << lessMoy;
+            }
+            if (i == 69)
+            {
+                 cout <<"+Moy : " << plusMoy;
+            }
+			if (i == 73)
+			{
+				 cout << "=1000 : " << ct;
+			}   
+        }
+        else
+        {
+            if ((i + 2) % 2 == 0 )
+            {
+                 cout << endl;
+            }
+               
+            cout << "#" << i << "\t" << tbRandRes[i] << "\tMoy:\t nd \t";   
+            
+            if (i == 63)
+            {
+                 cout << "-Occ : " << lessOcc;
+            }
+            if (i == 65)
+            {
+                 cout <<"+Occ : " << plusOcc;
+            }
+			if (i == 73)
+			{
+				 cout << "=1000 : " << ct;
+			}           
+        }
+    }
+
+    firstLast = false;
+    firstPrint = false;   
+    
+    cin.get();    
+}
+
+return 0;
+}
